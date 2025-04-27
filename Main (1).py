38f6225e40b4477dec4ad3c4ee128c4922c54c1e
TAY0001 <tayzy-wp22@student.tarc.edu.my>
2025-04-27 21:16:27 +0800

    Update Main (1).py

--- Main (1).py
+++ Main (1).py
@@ -12,6 +12,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import GradientBoostingClassifier
 from imblearn.over_sampling import SMOTE
 from joblib import load
+import joblib
 
 # --- Functions ---
 
