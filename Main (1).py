6176de00c982ca46f73abd14e410fe76e24b945b
TAY0001 <tayzy-wp22@student.tarc.edu.my>
2025-04-27 23:07:34 +0800

    Update Main (1).py

--- Main (1).py
+++ Main (1).py
@@ -10,6 +10,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.decomposition import PCA
 import joblib
 import os
 
