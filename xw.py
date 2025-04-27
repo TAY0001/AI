8d9148fb5ce5d3ef156a98244b1e6d8945d17ebe
TAY0001 <tayzy-wp22@student.tarc.edu.my>
2025-04-27 09:06:03 +0800

    Update xw.py

--- xw.py
+++ xw.py
@@ -11,7 +11,7 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.decomposition import PCA
 
-@st.cache_data
+@st.cache_resource
 def load_data():
     df = pd.read_csv(r"C:\Users\admin\Desktop\credit_risk_dataset.csv")
     return df
