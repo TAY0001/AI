ee611386ce3524fec47513743184a49a2b8f2650
TAY0001 <tayzy-wp22@student.tarc.edu.my>
2025-05-14 04:46:11 +0800

    Update Main2.py

--- Main2.py
+++ Main2.py
@@ -69,6 +69,8 @@ df = df.assign(
 # Data Split
 X = df.drop(columns=['loan_status'], axis=1)
 X = X.select_dtypes(include=[np.number])
+print("X_train shape:", X_train.shape)
+print("Input shape:", input_data_scaled.shape)
 y = df['loan_status']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
